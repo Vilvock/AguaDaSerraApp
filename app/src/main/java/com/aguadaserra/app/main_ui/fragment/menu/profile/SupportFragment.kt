8de222cb70f5470ea122feaa2a9ce55d8835ce95
Vilvock <luisvilvock@gmail.com>
2023-02-28 12:10:32 -0300

    ok ok

--- app/src/main/java/com/aguadaserra/app/main_ui/fragment/menu/profile/SupportFragment.kt
+++ app/src/main/java/com/aguadaserra/app/main_ui/fragment/menu/profile/SupportFragment.kt
@@ -7,30 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import com.aguadaserra.app.R
 import com.aguadaserra.app.global_ui.config_fragment.BaseFragment
-import kotlinx.android.synthetic.main.fragment_main_menu.*
-
 
 /**
  * A simple [Fragment] subclass.
  */
-class MainMenuFragment : BaseFragment() {
-
+class SupportFragment : BaseFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_menu, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        userData_ib.setOnClickListener {
-            navigation.navigate(R.id.action_mainMenuFragment_to_userAddressFragment)
-        }
-
+        return inflater.inflate(R.layout.fragment_support, container, false)
     }
 
 }
